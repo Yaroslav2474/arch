0ef4f796b9c4321f9aac2ac181dd1269ee045dd1
Yaroslav2474 <sherbyaroslav@yandex.ru>
2025-03-09 22:51:02 +0300

    com

--- code/main.go
+++ code/main.go
@@ -2,34 +2,41 @@ package main
 
 import "fmt"
 
+func remove(s []int, i int) []int {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
 func main() {
 	var students []string
 	var name string
 	var marks []int
 	var choise int
 	var mark int
+	var index int
 
 	for {
 		fmt.Print("\n1.Добавить студента и оценку \n2. Вывести студентов и их оценки \n3. Удалить студента и оценку \n0. Exit\n\nВведите цифру: ")
 		fmt.Scan(&choise)
-
-		if choise == 0 {
+		switch choise {
+		case 0:
 			break
-		}
-
-		if choise == 1 {
+		case 1:
 			fmt.Print("Введите имя студента: ")
 			fmt.Scan(&name)
 			students = append(students, name)
 			fmt.Print("Введите оценку студента: ")
 			fmt.Scan(&mark)
 			marks = append(marks, mark)
-		}
-
-		if choise == 2 {
+		case 2:
 			for i := 0; i < len(students); i++ {
-				fmt.Print(students[i], " имеет оценку ", marks[i])
+				fmt.Print(i+1, students[i], " имеет оценку ", marks[i])
 			}
+		case 3:
+			fmt.Print("Введите номер студента: ")
+			fmt.Scan(&index)
+			students = remove(students, index)
+			marks = remove(marks, index)
 		}
 
 	}
